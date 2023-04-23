81d7e759a760c26639b4b55acaf8d171c229f75f
leofdss <leonardo.f.desouzasilva@gmail.com>
2023-04-23 20:59:30 +0000

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'signal';
+
+  counter1 = signal(0);
+  counter2 = signal(10);
+
+  join = computed(() => {
+    return this.counter1() + this.counter2();
+  })
+
+  constructor() {
+    this.init();
+  }
+
+  init(): void {
+    effect(() => {
+      console.log(this.counter1())
+    });
+
+    const interval = setInterval(() => {
+      this.counter1.set(this.counter1() + 1)
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(interval)
+    }, 10 * 1000)
+  }
 }
